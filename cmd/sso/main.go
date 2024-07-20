3dfd69bb1819eb8d5f1e01a63a46027060be3cf7
nvsick <nvs.holy@gmail.com>
2024-07-20 15:53:07 +0300

    init

---
+++ cmd/sso/main.go
@@ -0,0 +1,11 @@
+package sso
+
+func main() {
+	// TODO: initialize config
+
+	// TODO: initialize logger
+
+	// TODO: initialize app
+
+	// TODO: run grpc server
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module sso
+
+go 1.22
+
+require github.com/nvsich/sso_protos v0.0.3 // indirect
