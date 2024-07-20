bed75355ac90ecb6c24db93c589b44341f771092
nvsick <nvs.holy@gmail.com>
2024-07-20 21:10:49 +0300

    add Auth handler logic

---
+++ internal/services/auth/auth.go
@@ -0,0 +1,52 @@
+package auth
+
+import (
+	"context"
+	"errors"
+	"log/slog"
+	"sso/internal/domain/models"
+	"time"
+)
+
+type Auth struct {
+	log          *slog.Logger
+	userSaver    UserSaver
+	userProvider UserProvider
+	appProvider  AppProvider
+	tokenTTL     time.Duration
+}
+
+type UserSaver interface {
+	SaveUser(ctx context.Context, email string, passHash []byte) (int64, error)
+}
+
+type UserProvider interface {
+	User(ctx context.Context, email string) (models.User, error)
+	IsAdmin(ctx context.Context, userID int64) (bool, error)
+}
+
+type AppProvider interface {
+	App(ctx context.Context, appId int64) (models.App, error)
+}
+
+func New(
+	log *slog.Logger,
+	userSaver UserSaver,
+	userProvider UserProvider,
+	appProvider AppProvider,
+	tokenTTL time.Duration,
+) *Auth {
+	return &Auth{
+		log:          log,
+		userSaver:    userSaver,
+		userProvider: userProvider,
+		appProvider:  appProvider,
+		tokenTTL:     tokenTTL,
+	}
+}
+
+var (
+	ErrInvalidCredentials = errors.New("invalid credentials")
+	ErrAppNotFound        = errors.New("app not found")
+	ErrUserExists         = errors.New("user already exists")
+)
---
+++ internal/services/auth/register.go
@@ -0,0 +1,42 @@
+package auth
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"golang.org/x/crypto/bcrypt"
+	"log/slog"
+	"sso/internal/storage"
+)
+
+func (auth *Auth) Register(ctx context.Context, email, password string) (int64, error) {
+	const op = "auth.Register"
+
+	log := auth.log.With(
+		slog.String("op", op),
+		slog.String("email", email),
+	)
+
+	log.Info("registering user")
+
+	passHash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.MinCost)
+	if err != nil {
+		log.Error("failed to hash password", err)
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+
+	userID, err := auth.userSaver.SaveUser(ctx, email, passHash)
+	if err != nil {
+		if errors.Is(err, storage.ErrUserExists) {
+			log.Info("user exists", err)
+			return 0, fmt.Errorf("%s: %w", op, ErrUserExists)
+		}
+
+		log.Error("failed to save user", err)
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+
+	log.Info("successfully registered user")
+
+	return userID, nil
+}
