fc2ed6968168f27cb457d7ccd751c657370677c5
nvsick <nvs.holy@gmail.com>
2024-08-23 15:13:31 +0300

    add config

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -1,7 +1,13 @@
-package sso
+package main
+
+import (
+	"fmt"
+	"sso/internal/config"
+)
 
 func main() {
-	// TODO: config
+	cfg := config.MustLoad()
+	fmt.Println(cfg)
 
 	// TODO: logger
 
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,61 @@
 package config
+
+import (
+	"flag"
+	"github.com/ilyakaznacheev/cleanenv"
+	"os"
+	"time"
+)
+
+type Config struct {
+	Env      string         `yaml:"env" env-required:"true"`
+	Postgres PostgresConfig `yaml:"postgres" env-required:"true"`
+	TokenTTL time.Duration  `yaml:"token_ttl" env-required:"true"`
+	GRPC     GRPCConfig     `yaml:"grpc" env-required:"true"`
+}
+
+type PostgresConfig struct {
+	DB       string `yaml:"POSTGRES_DB" env-required:"true"`
+	User     string `yaml:"POSTGRES_USER" env-required:"true"`
+	Password string `yaml:"POSTGRES_PASSWORD" env-required:"true"`
+	Host     string `yaml:"POSTGRES_HOST" env-required:"true"`
+	Port     int    `yaml:"POSTGRES_PORT" env-required:"true"`
+}
+
+type GRPCConfig struct {
+	Port    int
+	Timeout time.Duration
+}
+
+func MustLoad() *Config {
+	path := fetchConfigPath()
+
+	if path == "" {
+		panic("config file path is empty")
+	}
+
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		panic("config file not found")
+	}
+
+	var config Config
+
+	if err := cleanenv.ReadConfig(path, &config); err != nil {
+		panic("failed to parse config" + err.Error())
+	}
+
+	return &config
+}
+
+func fetchConfigPath() string {
+	var res string
+
+	flag.StringVar(&res, "config", "", "Path to config file")
+	flag.Parse()
+
+	if res == "" {
+		res = os.Getenv("CONFIG_PATH")
+	}
+
+	return res
+}
